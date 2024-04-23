a6741022a6e19b498ec046323dd88a1397241649
Inarixdono <jenielwtf@gmail.com>
2024-04-22 23:44:36 -0400

    Add documentation

--- src/models/Display.java
+++ src/models/Display.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 
 import interfaces.Representable;
 
+/**
+ * The Display class represents a display interface for interacting with the user.
+ * It provides methods for printing messages, displaying lists, and selecting items.
+ */
 public class Display {
     private Scanner scanner;
     private String reprType;
@@ -12,6 +16,13 @@ public class Display {
     private int displayLength;
     private ArrayList<Representable> list;
 
+    /**
+     * Constructs a Display object with the specified representation type, scanner, and list of items.
+     *
+     * @param reprType the representation type of the items
+     * @param scanner the scanner object used for user input
+     * @param list the list of items to be displayed
+     */
     public Display(String reprType, Scanner scanner, ArrayList<Representable> list) {
         this.list = list;
         this.isRunning = true;
@@ -20,41 +31,79 @@ public class Display {
         this.reprType = reprType;
     }
 
+    /**
+     * Returns the scanner object used for user input.
+     *
+     * @return the scanner object
+     */
     public Scanner getScanner() {
         return scanner;
     }
 
+    /**
+     * Checks if the display is running.
+     *
+     * @return true if the display is running, false otherwise
+     */
     public boolean isRunning() {
         return isRunning;
     }
 
+    /**
+     * Turns off the display.
+     */
     private void turnOff() {
         this.isRunning = false;
     }
 
+    /**
+     * Prints the specified message.
+     *
+     * @param message the message to be printed
+     */
     private void print(String message) {
         System.out.print(message);
     }
 
+    /**
+     * Prints the specified message followed by a new line.
+     *
+     * @param message the message to be printed
+     */
     private void println(String message) {
         System.out.println(message);
     }
 
+    /**
+     * Clears the console screen.
+     */
     private void clearConsole() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
+    /**
+     * Checks if the specified string is numeric.
+     *
+     * @param str the string to be checked
+     * @return true if the string is numeric, false otherwise
+     */
     private boolean isNumeric(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");
     }
 
+    /**
+     * Waits for the user to press enter.
+     */
     public void waitForUser()  {
         print("Press enter to continue...");
         scanner.nextLine();
         clearConsole();
     }
 
+    /**
+     * Prints the list of items.
+     */
     private void printList() {
         Representable item;
 
@@ -68,6 +117,11 @@ public class Display {
         println("-".repeat(displayLength) + "\n");
     }
 
+    /**
+     * Displays the specified message to the user.
+     *
+     * @param message the message to be displayed
+     */
     public void showMessage(String message) {
         clearConsole();
         println("-".repeat(displayLength) + "\n");
@@ -76,6 +130,12 @@ public class Display {
         waitForUser();
     }
 
+    /**
+     * Allows the user to select an item from the list.
+     *
+     * @param message the message to be displayed before selecting an item
+     * @return the selected item
+     */
     public Representable selectItem(String message) {
         int index;
         String input;
