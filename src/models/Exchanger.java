c41b7ec140fbe69aebc38a4bdf44814270707ceb
Inarixdono <jenielwtf@gmail.com>
2024-04-22 23:26:29 -0400

    added documentation

--- src/models/Exchanger.java
+++ src/models/Exchanger.java
@@ -6,10 +6,21 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import com.google.gson.JsonParser;
 
+/**
+ * The Exchanger class represents a currency exchanger that converts currency from one type to another.
+ */
 public class Exchanger {
 
     private double conversionResult;
 
+    /**
+     * Converts the specified amount of currency from one type to another.
+     *
+     * @param from The currency to convert from.
+     * @param to The currency to convert to.
+     * @throws IOException If an I/O error occurs while making the HTTP request.
+     * @throws InterruptedException If the thread is interrupted while waiting for the HTTP response.
+     */
     public void converTo(Currency from, Currency to) throws IOException, InterruptedException {
 
         var url = new UrlBuilder().getRequestURI(from, to);
